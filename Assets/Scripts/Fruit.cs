d2031848b1015c5eefe1a8478624896d54fe34d2
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2017-06-23 22:45:15 +0200

    last minute inheritance

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Fruit : Item {
+
+    public float spoil;
+
+    void FixedUpdate()
+    {
+        spoil++;
+        if(spoil >= 100)
+        {
+            this.gameObject.SetActive(false);
+            print (this.gameObject.name +"spoiled");
+        }
+    }
+}
